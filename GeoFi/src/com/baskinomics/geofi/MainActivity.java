6c4ad2904f6f844d29575e292f59be5d15f511a1
Sean Baskin <seanbaskin@gmail.com>
2013-08-20 15:28:46 -0400

    Added ErrorDialogFragment class

--- GeoFi/src/com/baskinomics/geofi/MainActivity.java
+++ GeoFi/src/com/baskinomics/geofi/MainActivity.java
@@ -1,20 +1,35 @@
 package com.baskinomics.geofi;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements 
+    LocationListener, 
+    GooglePlayServicesClient.ConnectionCallbacks, 
+    GooglePlayServicesClient.OnConnectionFailedListener {
 
     // Member declaration
     private GoogleMap mGoogleMap;
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+    // A request to connect to Location Services
+    private LocationRequest mLocationRequest;
+    // Stores the current instantiation of the location client in this object
+    private LocationClient mLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,4 +47,24 @@ public class MainActivity extends Activity {
     	inflater.inflate(R.menu.main_activity_actions, menu);
     	return super.onCreateOptionsMenu(menu);
 	}
+
+    // Define a DialogFragment that displays the error dialog
+    public static class ErrorDialogFragment extends DialogFragment {
+        // Global field to contain the error dialog
+        private Dialog mDialog;
+        // Default constructor. Sets the dialog field to null
+        public ErrorDialogFragment() {
+            super();
+            mDialog = null;
+        }
+        // Set the dialog to display
+        public void setDialog(Dialog dialog) {
+            mDialog = dialog;
+        }
+        // Return a Dialog to the DialogFragment.
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            return mDialog;
+        }
+    }
 }
