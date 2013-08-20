e1aa70fe16be3895ff8eb9cf7090f6395b43a2f7
Sean Baskin <seanbaskin@gmail.com>
2013-08-20 15:10:50 -0400

    Added My Location layer to map

--- GeoFi/src/com/baskinomics/geofi/MainActivity.java
+++ GeoFi/src/com/baskinomics/geofi/MainActivity.java
@@ -1,15 +1,35 @@
 package com.baskinomics.geofi;
 
-import android.app.Activity;
-import android.os.Bundle;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 
-public class MainActivity extends Activity
-{
-    /** Called when the activity is first created. */
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    // Member declaration
+    private GoogleMap mGoogleMap;
+
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        mGoogleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        // Enable My Location Layer
+        mGoogleMap.setMyLocationEnabled(true);
     }
+
+    @Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+    	// Inflate the menu items for use in the action bar
+    	MenuInflater inflater = getMenuInflater();
+    	inflater.inflate(R.menu.main_activity_actions, menu);
+    	return super.onCreateOptionsMenu(menu);
+	}
 }
